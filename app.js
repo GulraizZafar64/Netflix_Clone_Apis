c1f0a4ec23f2b4e351a68e5d94aeed3192bfbe51
GulraizZafar64 <138650015+GulraizZafar64@users.noreply.github.com>
2023-07-06 18:41:19 +0500

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,13 @@ app.use("/api/v1",user)
 
 //middleware for error
 app.use(errorMiddleware)
-app.get("/",(req,res)=>{
-  res.send("<h2>Welcome to our API</h2>")
-})
+app.get('/api/users', (req, res) => {
+
+  const users = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Jane' }
+  ];
+
+  res.json(users);
+});
 module.exports=app
