5b2f58a0071c36b84d27415500d8a3467108f84c
GulraizZafar64 <138650015+GulraizZafar64@users.noreply.github.com>
2023-07-10 15:24:15 +0500

    update

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose=require("mongoose")
 
-
+const uri="mongodb+srv://abdulrafay9797:Eueu_6464@netflix.2a03rl4.mongodb.net/?retryWrites=true&w=majority"
 const connectDatabase=( )=>{
     mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.DB_URI,{useUnifiedTopology:true,
+    mongoose.connect(uri,{useUnifiedTopology:true,
     useNewUrlParser:true}).then((data)=>{
     console.log(`Mongodb connected with server:${data.connection.host}`);
  })
