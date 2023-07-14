6d02c18d78115b00309d6ffff5dcaded16e66ca6
GulraizZafar64 <138650015+GulraizZafar64@users.noreply.github.com>
2023-07-14 15:31:53 +0500

    still cors error

--- app.js
+++ app.js
@@ -15,12 +15,17 @@ app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}))
 // app.use(cors());
 app.use(cors({
-    origin: 'http://localhost:3000'
-}));
-
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 
+  }));
+  
+  const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // Some legacy browsers (e.g., IE11, various SmartTVs) choke on 204
+  };
 //routs imports
 const user=require("./routes/userRoute")
-app.use("/api/v1",user)
+app.use("/api/v1", cors(corsOptions),user)
 
 //middleware for error
 app.use(errorMiddleware)
