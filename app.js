a4e5168667668bdc04d2a78cc12a51d4ca548d25
GulraizZafar64 <138650015+GulraizZafar64@users.noreply.github.com>
2023-07-14 15:50:59 +0500

    Update app.js

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ dotenv.config({path:"config/config.env"})
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}))
-// app.use(cors());
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors());
+// app.use(cors({
+//     origin: '*'
+// }));
 
 //routs imports
 const user=require("./routes/userRoute")
