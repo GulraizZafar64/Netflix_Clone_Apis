7f69dc0ed81b9f8d97084bce527aa983c6006d4b
GulraizZafar64 <138650015+GulraizZafar64@users.noreply.github.com>
2023-07-14 15:09:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ dotenv.config({path:"config/config.env"})
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}))
-app.use(cors());
+// app.use(cors());
 app.use(cors({
     origin: '*'
 }));
