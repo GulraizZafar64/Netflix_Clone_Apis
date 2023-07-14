f3ffb5dc4134741cfe7ccbc4ef7302e410d11845
GulraizZafar64 <138650015+GulraizZafar64@users.noreply.github.com>
2023-07-14 16:25:10 +0500

    some change

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const errorMiddleware=require('./middleware/error')
 const bodyParser=require('body-parser')
 const dotenv=require('dotenv')
 const cors = require('cors');
-const { createProxyMiddleware } = require('http-proxy-middleware')
+
 
 //config
 dotenv.config({path:"config/config.env"})
@@ -14,21 +14,11 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors());
-// app.use(cors({
-//     origin: '*'
-// }));
 app.use(cors({
-  origin: 'http://localhost:3000' // Replace with the actual URL of your React app
+  origin: '*'
 }));
-
-app.use(
-    '/api/v1',
-    createProxyMiddleware({
-      target: 'http://localhost:3000',
-      changeOrigin: true,
-    }),
-  )
 //routs imports
+
 const user=require("./routes/userRoute")
 app.use("/api/v1",user)
 
